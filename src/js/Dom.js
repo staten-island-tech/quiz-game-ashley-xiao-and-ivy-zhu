bbd2f6d2dab25c0e46dcc1f3ac6683ea861efa0f
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-04-05 17:37:25 -0400

    -

--- src/js/Dom.js
+++ src/js/Dom.js
@@ -1,12 +1,14 @@
-const startButton =  document.getElementById('start-btn')
-const questionElement = document.getElementById('question')
-const answerContainer = document.getElementById('answer-choices')
-const answerChoices =  document.getElementsByClassName('answer')
-const nextButton = document.getElementById('next-btn')
-const submitButton = document.getElementById('submit')
-const endResults = document.getElementById('results')
-const retakeButton = document.getElementById('retake')
-const scoreResult = document.getElementById('iq-result')
-const resultComment = document.getElementById('result-comment')
+const DOMselectors = {
+    startButton: document.querySelector("#start-btn"),
+    questionElement: document.querySelector("#question"),
+    answerContainer: document.querySelector("#answer-choices"),
+    answerChoices: document.getElementsByClassName('answer'),
+    nextButton: document.querySelector("#next-btn"),
+    submitButton: document.querySelector("#submit"),
+    endResults: document.querySelector("#results"),
+    retakeButton: document.querySelector("#retake"),
+    scoreResult: document.querySelector("#iq-result"),
+    resultComment: document.querySelector("#result-comment"),
+};
 
-export { startButton, questionElement, answerContainer, answerChoices, nextButton, submitButton, endResults, retakeButton, scoreResult, resultComment };
+export { DOMselectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,24 @@
 import { quizQuestions } from "./questions"
-import { startButton, questionElement, answerContainer, answerChoices, nextButton, submitButton, endResults, retakeButton, scoreResult, resultComment } from "./dom"
+import { DOMselectors } from "./Dom"
 
 let currentQuestion
 let score
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
+DOMselectors.startButton.addEventListener('click', startGame)
+
+DOMselectors.nextButton.addEventListener('click', () => {
     currentQuestion++ //adds 1 to the current index when next button is clicked (changes the question)
     nextQuestion()
 })
-submitButton.addEventListener('click', showResults)
-retakeButton.addEventListener('click', retakeQuiz)
 
+DOMselectors.submitButton.addEventListener('click', showResults)
+
+DOMselectors.retakeButton.addEventListener('click', retakeQuiz)
 
 function startGame() { //what happens when you press the start button or restart button (sets the first question)
-    startButton.classList.add('hidden')
-    questionElement.classList.remove('hidden')
-    Array.from(answerChoices).forEach(function(answers){
+    DOMselectors.startButton.classList.add('hidden')
+    DOMselectors.questionElement.classList.remove('hidden')
+    Array.from(DOMselectors.answerChoices).forEach(function(answers){
         answers.classList.remove('hidden')
     }); //adding and removing hidden classes to different elements so the question appears
     currentQuestion = 0 //acts as the index to be used later
@@ -30,13 +32,13 @@ function nextQuestion() { //sets the question
 }
 
 function showQuestion(question) { //changes the text on each element to match new question
-    questionElement.innerText = question.question
+    DOMselectors.questionElement.innerText = question.question
     question.answers.forEach(answer => {
         const button = document.createElement('div') //adding answer choices for the question
         button.innerText = answer.option //text in the buttons created as the answer choices
         button.classList.add('answer')
         button.addEventListener('click', showButton)
-        answerContainer.appendChild(button) //adds child to the end of a parent element
+        DOMselectors.answerContainer.appendChild(button) //adds child to the end of a parent element
 
         button.addEventListener('click', scores)
         function scores() {
@@ -52,52 +54,48 @@ function showQuestion(question) { //changes the text on each element to match ne
 }
 
 function changeQuestion() { //goes to next question after pressing next button
-    nextButton.classList.add('hidden')
-    while (answerContainer.firstChild) {
-        answerContainer.removeChild(answerContainer.firstChild) //removes previous quesion (first child) so that only the new question stays
+    DOMselectors.nextButton.classList.add('hidden')
+    while (DOMselectors.answerContainer.firstChild) {
+        DOMselectors.answerContainer.removeChild(DOMselectors.answerContainer.firstChild) //removes previous quesion (first child) so that only the new question stays
     }
     
 }
 
 function showButton() { //shows either submit button or next button
     if (currentQuestion === quizQuestions.length -1) {
-        submitButton.classList.remove('hidden')
+        DOMselectors.submitButton.classList.remove('hidden')
     } else {
-        nextButton.classList.remove('hidden')
+        DOMselectors.nextButton.classList.remove('hidden')
     }
 }
 
 function showResults() {
-    questionElement.classList.add('hidden')
-    while (answerContainer.firstChild) {
-        answerContainer.removeChild(answerContainer.firstChild)
+    DOMselectors.questionElement.classList.add('hidden')
+    while (DOMselectors.answerContainer.firstChild) {
+        DOMselectors.answerContainer.removeChild(DOMselectors.answerContainer.firstChild)
     }
-    endResults.classList.remove('hidden')
-    submitButton.classList.add('hidden')
+    DOMselectors.endResults.classList.remove('hidden')
+    DOMselectors.submitButton.classList.add('hidden')
+
     if (score > 6) {
-        scoreResult.innerText = 170 + score
-        resultComment.innerText = "Wow! You're as smart as Einstein."
-    } else if (score < 3) {
-        scoreResult.innerText = 80 + score
-        resultComment.innerText = "Not bad! A little below average."
+        DOMselectors.scoreResult.innerText = 170 + score
+        DOMselectors.resultComment.innerText = "Wow! You're as smart as Einstein."
+    } else if (score === 0){
+        DOMselectors.scoreResult.innerText = 65
+        DOMselectors.resultComment.innerText = "Pretty good for such a hard test."
+    } else if (score < 0) {
+        DOMselectors.scoreResult.innerText = 60 + score
+        DOMselectors.resultComment.innerText = "Don't worry, my IQ is lower."
     } else {
-        scoreResult.innerText = 100 + score
-        resultComment.innerText = "Wow, how did you even solve those questions?"
-    }
-    if (score === 0){
-        scoreResult.innerText = 65
-        resultComment.innerText = "Pretty good for such a hard test."
-    }
-    if (score < 0) {
-        scoreResult.innerText = 60 + score
-        resultComment.innerText = "Don't worry, my IQ is lower."
-    }
+        DOMselectors.scoreResult.innerText = 100 + score
+        DOMselectors.resultComment.innerText = "Wow, how did you even solve those questions?"
+    } 
 }
 
 function retakeQuiz() {
     results.classList.add('hidden')
-    questionElement.classList.remove('hidden')
-    Array.from(answerChoices).forEach(function(answers){
+    DOMselectors.questionElement.classList.remove('hidden')
+    Array.from(DOMselectors.answerChoices).forEach(function(answers){
         answers.classList.remove('hidden')
     }); 
     currentQuestion = 0 
